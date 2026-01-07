7d8c00761d26ff824a28785c24f8ce9200351de2
anbbch <155988779+anbbch@users.noreply.github.com>
2026-01-07 17:33:04 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -16,5 +16,20 @@ cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGN
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
+# Utilisateurs
+cur.execute("INSERT INTO utilisateurs (username, password, role) VALUES (?, ?, ?)",("admin", "password", "admin"))
+cur.execute("INSERT INTO utilisateurs (username, password, role) VALUES (?, ?, ?)",("user", "12345", "user"))
+
+# Livres (exemples)
+livres = [
+    ("Le Petit Prince", "Antoine de Saint-Exupéry", "9782070612758", 3, 3),
+    ("1984", "George Orwell", "9780451524935", 2, 2),
+    ("L'Étranger", "Albert Camus", "9782070360024", 1, 1),
+    ("Harry Potter à l'école des sorciers", "J.K. Rowling", "9782070584628", 4, 4),
+    ("Les Misérables", "Victor Hugo", "9782253004226", 2, 2),
+]
+
+cur.executemany("INSERT INTO livres (titre, auteur, isbn, stock_total, stock_disponible) VALUES (?, ?, ?, ?, ?)",livres)
+
 connection.commit()
 connection.close()
